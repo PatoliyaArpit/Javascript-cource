90f1dccb23d87b5d903b87eb8a8d28ff62e3cb4a
Arpit Patoliya <154015590+PatoliyaArpit@users.noreply.github.com>
2024-02-05 23:27:31 +0530

    Create 04_primitive_nonprimitive.js

---
+++ 04_primitive_nonprimitive.js
@@ -0,0 +1,20 @@
+//nn bb ss u primitive in js
+let a = null
+let b = 364
+let c = true
+let d = BigInt("567")+BigInt("3")
+let e = "Kenil"
+let f = Symbol("i an nice symbol")
+let g = undefined
+console.log(a, b, c, d, e, f, g)
+console.log(typeof c)
+
+
+//objects  non-primitive data
+const item ={
+    "Arpit":"hello",
+    "kenil":true,
+    "rutil":80,
+    "pavan":undefined
+}
+console.log(item["rutil"])
