7317ddc6bbcf089f16fda5bcfcd8c782f7d5ad62
Arpit Patoliya <154015590+PatoliyaArpit@users.noreply.github.com>
2024-02-05 23:26:03 +0530

    Create 02_var.js

---
+++ 02_var.js
@@ -0,0 +1,11 @@
+console.log("jay swaminarayan");
+let a=50// containt
+console.log(a)
+a="Arpit"
+console.log(a)
+//  let 8a="Arpit"//not allow
+let ArPit=8
+console.log(ArPit)
+let aRpit=11
+console.log(aRpit)
+// let a1=prompt("Enter no?");
