cf788c3801bbf234385153f78fa14464d298fc03
Arpit Patoliya <154015590+PatoliyaArpit@users.noreply.github.com>
2024-02-05 23:26:43 +0530

    Create 03_primitive_nonprimitive.js

---
+++ 03_primitive_nonprimitive.js
@@ -0,0 +1,11 @@
+    console.log("03 var,let and const")
+    // var a=5   // both is same to be not create arror,let in create arror
+    // var a="A"
+    const Author="Arpit1"
+    // let Author="kenil1"//throws an errors alrady create a variable because cannot a change
+    let b="Arpit"//out side a boak in print
+    {
+        let b ="kenil"//inside a boak print
+        console.log(b)
+    }
+    console.log(b)
